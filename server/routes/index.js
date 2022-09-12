67ce0b4d1123324a94c3962d5fa3c1267f54474a
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-09-12 17:44:25 +0300

    del .idea

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = new Router()
 
 const deviceRouter = require('./deviceRouter')
 const userRouter = require('./userRouter')
-const brandRouter = require('./deviceRouter')
+const brandRouter = require('./brandRouter')
 const typeRouter = require('./typeRouter')
 
 
